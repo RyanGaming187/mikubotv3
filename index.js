2453d7f5e73e13a3a860c519350598c2f2500ef9
RyanGaming187 <79962188+RyanGaming187@users.noreply.github.com>
2021-03-04 09:15:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'Nama bot'; 
-const instagram = 'Instagram kamu'; 
+const BotName = 'Miku Bot'; 
+const instagram = 'rayyan.afif_l'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Nama kamu\n' // Nama kamu
-            + 'ORG:Nama bot;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6280000000000:+62 000-0000-0000\n' //Nomor whatsapp kamu
+            + 'FN:Wafiq Haiqal\n' // Nama kamu
+            + 'ORG:Miku Bot;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=601128692700:+60 11-2869-2700\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
